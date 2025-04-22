009e10626994a694ddc615da5fed54e9ba83dc1f
Excalibuurr <legendaryyusuf8@gmail.com>
2025-04-23 01:38:29 +0530

    model trainer and mlflow commit

--- test_mongodb.py
+++ test_mongodb.py
@@ -2,7 +2,7 @@
 from pymongo.mongo_client import MongoClient
 from pymongo.server_api import ServerApi
 
-uri = "mongodb+srv://legendaryyusuf8:XLV0ET0UJC2N4air@cluster0.siski.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+uri = "mongodb+srv://legendaryyusuf8:zVimUMfrYXrmRJcY@cluster0.siski.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
 
 # Create a new client and connect to the server
 client = MongoClient(uri, server_api=ServerApi('1'))
